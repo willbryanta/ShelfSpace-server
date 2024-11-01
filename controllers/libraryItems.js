82937aa3debe931d755d066106873fa8c01c9782
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-01 22:29:48 +1100

    edit: strict params from create library item form

--- controllers/libraryItems.js
+++ controllers/libraryItems.js
@@ -4,13 +4,19 @@ const User = require("../models/user.js")
 const LibraryItem = require("../models/LibraryItem.js")
 const authenticateUser = require("../middleware/authenticateUser.js")
 
-router.post('/', authenticateUser, async (req, res) => {
-    try {
-        const createdLibraryItem = await LibraryItem.create(req.body)
-        res.status(201).json(createdLibraryItem)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+router.post("/", authenticateUser, async (req, res) => {
+	try {
+		const createdLibraryItem = await LibraryItem.create({
+			name: req.body.name,
+			description: req.body.description,
+			publicationDate: req.body.publicationDate,
+			author: req.body.author,
+			reviews: [],
+		})
+		res.status(201).json(createdLibraryItem)
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
 })
 
 module.exports = router
