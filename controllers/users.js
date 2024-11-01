77507b8571af6159297cf4e813932f9afc039a43
Eddie <edwardchen_97@hotmail.com>
2024-11-01 12:42:52 +1100

    add create list for user

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const User = require("../models/user.js")
 const router = express.Router()
 
 router.use(authenticateUser)
+
 router.post("/:userId/lists", async (req, res) => {
 	try {
 		const targetUser = await User.findById(req.params.userId)
@@ -17,9 +18,22 @@ router.post("/:userId/lists", async (req, res) => {
 				error: " You don't have permission to create this new list!",
 			})
         }
+
+        const newList = {
+            listName: req.body.listName,
+            items: req.body.items
+        }
+
+        targetUser.lists.push(newList)
+        await targetUser.save()
+
+        return res.status(201).json(targetUser.lists)
         
-        const newList = 
+        
     } catch (error) {
         return res.status(500).json({error: " The server fell down, try again later!"})
     }
 })
+
+
+module.exports = router
