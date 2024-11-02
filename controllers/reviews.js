20ecfac41cf738632491c00b7ddb667b1e76f78f
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-02 12:54:20 +1100

    adds DELETE route for a review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -30,4 +30,21 @@ router.put("/:reviewId", authenticateUser, async (req, res) => {
 	}
 })
 
+router.delete('/:reviewId', authenticateUser, async (req, res) => {
+	try {
+		const deletedReview = await Review.findByIdAndDelete(req.params.reviewId);
+		if (!deletedReview) {
+			res.status(404);
+			throw new Error('Review not found.');
+		}
+		res.status(200).json(deletedReview);
+	} catch (error) {
+		if (res.statusCode === 404) {
+			res.json({ error: error.message });
+		} else {
+			res.status(500).json({ error: error.message });
+		}
+	}
+});
+
 module.exports = router
