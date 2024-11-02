3e12beded34c1626771131a13847febc3deb594e
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-02 12:15:33 +1100

    adds DELETE route for a single item

--- controllers/library.js
+++ controllers/library.js
@@ -62,4 +62,21 @@ router.post("/", authenticateUser, async (req, res) => {
 	}
 })
 
+router.get('/:itemId', async (req, res) => {
+	try {
+		const deletedItem = await LibraryItem.findByIdAndDelete(req.params.itemId);
+		if (!deletedItem) {
+			res.status(404);
+			throw new Error('Library item not found.');
+		}
+		res.status(200).json(deletedItem);
+	} catch (error) {
+		if (res.statusCode === 404) {
+			res.json({ error: error.message });
+		} else {
+			res.status(500).json({ error: error.message });
+		}
+	}
+});
+
 module.exports = router
