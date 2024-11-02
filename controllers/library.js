caaf1f6925c832f8ba24d1c96933b35db772100e
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-02 12:10:27 +1100

    adds GET route for a single item

--- controllers/library.js
+++ controllers/library.js
@@ -62,4 +62,21 @@ router.post("/", authenticateUser, async (req, res) => {
 	}
 })
 
+router.get('/:itemId', async (req, res) => {
+	try {
+		const foundItem = await LibraryItem.findById(req.params.itemId);
+		if (!foundItem) {
+			res.status(404)
+			throw new Error('Library item not found.')
+		}
+		res.status(200).json(foundItem)
+	} catch (error) {
+		if (res.statusCode === 404) {
+			res.json({ error: error.message });
+		} else {
+			res.status(500).json({ error: error.message });
+		}
+	}
+})
+
 module.exports = router
