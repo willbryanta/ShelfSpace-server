97611c44efcbe0fc069dc06887d8d35b2c51b9d0
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-02 12:40:06 +1100

    create: post route for review

---
+++ controllers/reviews.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const authenticateUser = require("../middleware/authenticateUser.js")
+const User = require("../models/User.js")
+const Review = require("../models/Review.js")
+const router = express.Router()
+
+router.put("/:reviewId", authenticateUser, async (req, res) => {
+	try {
+		const review = await Review.findByIdAndUpdate(
+			req.params.reviewId,
+			{
+				title: req.body.title,
+				description: req.body.description,
+				rating: req.body.rating,
+			},
+			{ new: true }
+		)
+
+		if (!review) {
+			return res
+				.status(404)
+				.json({ error: `Unfortunately we couldn't find that review` })
+		}
+
+		item = await review.populate("author").execPopulate()
+
+		res.status(200).json({ error: item })
+	} catch (error) {
+		res.status(500).json(error.message)
+	}
+})
+
+router.post("/", authenticateUser, async (req, res) => {
+	try {
+		const createdReview = Review.create({
+			title: req.body.title,
+			description: req.body.description,
+			rating: req.body.rating,
+			author: req.body.author,
+		})
+
+		if (!createdReview) {
+			res
+				.status(500)
+				.json({ error: `Unfortunately we couldn't create that review` })
+		}
+
+		res.status(200).json(createdReview)
+	} catch (error) {
+		return res.status(500).json(error.message)
+	}
+})
+
+module.exports = router
